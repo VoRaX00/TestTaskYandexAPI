ae2c765f71a998a9b6fbd11039bb62bbd778ea2b
nikita <nikitakerzhik@gmail.com>
2024-09-17 03:43:35 +1000

    edit base and index.html

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from app import views
 
+app_name = 'app'
+
 urlpatterns = [
-    path('view-files', views.view_files, name='view_files'),
-    path('download-file', views.download_file, name='download_file')
+    path('view-files', views.view_files, name='view-files'),
+    path('download-file', views.download_file, name='download-file')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def view_files(request):
-    return render(request, '')
+    return render(request, 'index.html')
 
 def download_file(request):
-    return render(request, '')
+    return render(request, 'index.html')
